66029c46e2c82635d744cd82c64012729fca4fa6
hr0303 <111736573+hr0303@users.noreply.github.com>
2024-02-16 21:29:06 +0530

    Update RemoveSpecialChar.java

--- RemoveSpecialChar.java
+++ RemoveSpecialChar.java
@@ -1,8 +1,19 @@
+
 public class RemoveSpecialChar {
     public static void main(String[] args) {
         String s="hello world!, hello universe!!";
         String s1=s.replaceAll("hello","hi");
         System.out.println(s1);
+
+    }
+}
+
+//Remove Junk/Special Chars in a String
+public class RemoveSpecialChar {
+    public static void main(String[] args) {
+        String s="&h&e$l$l#o#";
+        s=s.replaceAll("[^a-zA-Z0-9]","");
+        System.out.println(s);
 
     }
 }
