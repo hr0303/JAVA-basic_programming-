2dd9e40fc7a280fb94f6f4b0f64e47c4d9127729
hr0303 <111736573+hr0303@users.noreply.github.com>
2024-02-16 21:27:44 +0530

    Create RemoveSpecialChar.java

---
+++ RemoveSpecialChar.java
@@ -0,0 +1,8 @@
+public class RemoveSpecialChar {
+    public static void main(String[] args) {
+        String s="hello world!, hello universe!!";
+        String s1=s.replaceAll("hello","hi");
+        System.out.println(s1);
+
+    }
+}
