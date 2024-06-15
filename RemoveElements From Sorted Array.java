eb68a3b6e5c7b6688836260281941406f277edf0
hr0303 <111736573+hr0303@users.noreply.github.com>
2024-06-16 00:03:44 +0530

    Remove Duplicate Elements From Sorted Array.java

--- RemoveElements From Sorted Array.java
+++ RemoveElements From Sorted Array.java
@@ -1,3 +1,12 @@
+// Given an integer array nums sorted in non-decreasing order, 
+// remove the duplicates in-place such that each unique element appears only once. 
+// The relative order of the elements should be kept the same. 
+// Then return the number of unique elements in nums.
+// Input: nums = [0,0,1,1,1,2,2,3,3,4]
+// Output: 5, nums = [0,1,2,3,4,_,_,_,_,_]
+// Explanation: Your function should return k = 5, with the first five elements of nums being 0, 1, 2, 3, and 4 respectively.
+// It does not matter what you leave beyond the returned k (hence they are underscores).
+    
 class Solution {
     public int removeDuplicates(int[] nums) {
         int newLength = 1;
@@ -10,3 +19,5 @@ class Solution {
         return newLength;
     }
 }
+
+// Time Complexity : O(n)
