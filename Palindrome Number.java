dd82964599b0b13c830e71ecc641ca1ddb7bfa38
hr0303 <111736573+hr0303@users.noreply.github.com>
2024-07-18 22:48:30 +0530

    Update Palindrome Number.java

--- Palindrome Number.java
+++ Palindrome Number.java
@@ -20,3 +20,15 @@ class Solution {
     }
 }
 // Time complexity: O(log n)
+
+// 1. How did you approach it?
+//     idea is to find the the face value of the number and then multiply it with repective place value. 
+//     To find the face value given number is divided with 10  and remainder is stored in other variable as the reverse number.
+// 2. What challenges did you face?
+//     Determining the constraints where pallindrome numb could not be found.
+// 3. Did you reach the most optimal solution?
+//     Yes, Time Complexity is O(log n)
+// 4. Practical aspects of this question, real world usage.
+//     Pallindrome sequence in genetic material have great importance 
+// 5. Did this question match with any existing pattern you already know?
+//     no.
